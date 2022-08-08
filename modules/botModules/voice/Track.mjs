adebbb896ee2d64716a148579da120ac666a363f
DjesonPV <djeson.pascalvalette@gmail.com>
2022-08-08 23:56:55 +0200

    Direct use of FFMPEG to decode the video got by YTDLP
    Need FFmpeg installed on the host machine, but now allow to use `seek`, useful when you want to only play part of a video
    
    Modified the yt-dlp calls so they never try to write anything on disk, simply use it as a powerful data getter.
    
    Removed unused `ffmpeg` and `ffmpeg-static` modules, as I directly use ffmpeg on the machine and use the cli syntax.

--- modules/botModules/voice/Track.mjs
+++ modules/botModules/voice/Track.mjs
@@ -1,9 +1,11 @@
 import * as DiscordJsVoice from '@discordjs/voice';
 import * as fs from 'fs';
+import * as ChildProcess from 'child_process';
+
 import ytdl from 'youtube-dl-exec';
 import { YouTubeLiveStream } from 'ytls';
-import * as LANG from '../../Language.mjs';
 
+import * as LANG from '../../Language.mjs';
 import * as MP3Files from "./MP3Files.mjs";
 
 export default class Track {
@@ -29,8 +31,9 @@ export default class Track {
                 const fileURL = (await ytdl.exec(
                     this.url,
                     {
-                        format: 'bestaudio/bestaudio*',
-                        getUrl : true,
+                        format: 'bestaudio.1/bestaudio*.2/best.2',
+                        print: 'urls',
+                        simulate: true,
                     }
                 ).catch((reason)=>{reject(LANG.ERROR_NO_AUDIO_MEDIA)}))?.stdout;
 
@@ -52,15 +55,27 @@ export default class Track {
                 // _____________________________________________________________
                 // File is not a YouTube Livestream
                 else { 
-                    const process = ytdl.exec(
-                        fileURL,
-                        {
-                            output: '-',
-                            format : 'bestaudio/bestaudio*',
-                        },
-                        { stdio: ['ignore', 'pipe', 'ignore'] }
-                    );
-    
+                    // Any http|https url with a query string either t or start in seconds
+                    const seekTime = Math.floor(this.url.match(/https?:\/\/.*?\/.*?(?:\?|\&)(?:t|start)=(\d+)s?/)?.[1]) ?? 0;
+
+                    const ffmpegArgs = [
+                        '-reconnect', '1',
+                        '-reconnect_streamed', '1',
+                        '-reconnect_delay_max', '5',
+                    ].concat(seekTime > 0 ?[
+                        '-ss', `${seekTime}`
+                    ]:[]).concat([
+                        '-i', fileURL,
+                        '-analyzeduration', '0',
+                        '-loglevel', '0',
+                        '-ar', '48000',
+                        '-ac', '2',
+                        '-f', 'opus',
+                        '-acodec', "libopus",
+                        'pipe:1'
+                    ]);
+
+                    const process = ChildProcess.spawn('ffmpeg', ffmpegArgs, { windowsHide: true, shell: false });
                     const stream = process.stdout;
 
                     if (!stream) {
@@ -69,16 +84,15 @@ export default class Track {
                         return;
                     }
 
-                    process
-                        .once('spawn', () => {
+                    try {
+                        process.once('spawn', () => {
                             resolve(probeAndCreateAudioResource(stream, this));
-                        })
-                        .catch((error) => {
-                            if (!process.killed) process.kill();
-                            stream.resume();
-                            reject(error);
-                        })
-                    ;
+                        });
+                    } catch (error) {
+                        if (!process.killed) process.kill();
+                        stream.resume();
+                        reject(error);
+                    }
                 }
             } 
             // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
@@ -121,8 +135,10 @@ async function fromYTDLP(url, methods) {
     const info  = await ytdl.exec(
         url,
         {
-            skipDownload: true,
-            addMetadata: true,
+            embedMetadata: true,
+            noEmbedChapters: true,
+            noEmbedInfoJson: true,
+            simulate: true,
             dumpSingleJson: true,
         }).catch(error => {;});
 
@@ -135,7 +151,7 @@ async function fromYTDLP(url, methods) {
 
             // Data use in the MusicPlayer Embed
             title: parsedInfo.fulltitle || parsedInfo.title,
-            author: `${parsedInfo.extractor_key} • ${parsedInfo.channel ?? parsedInfo.artist ?? parsedInfo.uploader}`,
+            author: `${parsedInfo.webpage_url_domain} • ${parsedInfo.channel ?? parsedInfo.artist ?? parsedInfo.uploader ?? parsedInfo.creator}`,
             duration: parsedInfo.duration,
             videoThumbnail: parsedInfo.thumbnail,
             videoURL: parsedInfo.webpage_url,
